f9527eaa48626dd98d72931a83d504ac2010f0d5
Nasrul Hazim <nasrulhazim.m@gmail.com>
2020-02-29 16:56:59 +0800

    Added helpers

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,1 +1,37 @@
 <?php
+
+/*
+ * Check Kerberos Auth Status
+ */
+if (! function_exists('isKerberosEnabled')) {
+    function isKerberosEnabled()
+    {
+        return config('auth.kerberos.enabled');
+    }
+}
+
+/*
+ * Get Kerberos Auth User
+ */
+if (! function_exists('getKerberosUser')) {
+    function getKerberosUser()
+    {
+        return config('kerberos.model')::query()
+            ->where(config('kerberos.identifier'), getUsernameFromRequest())
+            ->firstOrFail();
+    }
+}
+
+/*
+ * Get Username from Header
+ */
+if (! function_exists('getUsernameFromRequest')) {
+    function getUsernameFromRequest()
+    {
+        return strtolower(
+            request()->header(
+                config('kerberos.header_key')
+            )
+        );
+    }
+}
